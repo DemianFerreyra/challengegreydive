210b423712c2f6aa28ae47f89d95f4ae812ee0cb
DemianFerreyra <demiancra@gmail.com>
2022-11-25 15:48:52 -0300

    app working, just css left

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const client = window.location.pathname;
+  const [ClientTest, setClientTest] = useState(Array<any>);
+  function GetData(){
+    fetch("/db.json")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data){
+      setClientTest(data.filter((e:any) => e.cliente === client.slice(1)));
+      console.log(data.filter((e:any) => e.cliente === client.slice(1)));
+    })
+  }
+
+  useEffect(() => {
+    GetData();
+  }, []);
+
+  function createMarkup(transcripcion: string) {
+    return {__html: transcripcion};
+  }
+
+
   return (
-    <div className="App">
-    </div>
+    <header className="App">
+      {
+        ClientTest.length === 0?(
+          <h1>Buscando cliente...</h1>
+        ):(
+          <>
+             {
+              ClientTest[0]?(<article className='TestingArea'>
+                <section className='ClientArea'>
+                <h2>{ClientTest[0].cliente}</h2>
+                <h2>Test: Test de usabilidad en el sitio web</h2>
+                <video src={ClientTest[0].linkVideo}></video>
+                </section>
+                <section className='Transcription'>
+                  <h1>Transcripción</h1>
+                  <div dangerouslySetInnerHTML={createMarkup(ClientTest[0].transcripcion)}></div>
+                </section>
+                <section className='Tareas'>
+                   <h1>Tareas</h1>
+                   <p>Escenario:{ClientTest[0].escenario}</p>
+                   {
+                    ClientTest[0].preguntas.map((e:any, index: any) =>(<section key={index}>
+                        <h1>Tarea {index}</h1>
+                        <h1>{e.texto}</h1>
+                        <p>Duracion de la tarea: {e.tiempo}</p>
+                    </section>))
+                   }
+                </section>
+              </article>):(<h1>{`el cliente: ${client.slice(1)} no existe`}</h1>)
+             }
+          </>
+        )
+      }
+    </header>
   );
 }
+
 
 export default App;

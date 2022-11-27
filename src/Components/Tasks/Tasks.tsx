e74a94d51203c8dfc7b5e532b420e9886968de29
DemianFerreyra <demiancra@gmail.com>
2022-11-27 17:12:30 -0300

    Tasks CSS

--- src/Components/Tasks/Tasks.tsx
+++ src/Components/Tasks/Tasks.tsx
@@ -8,16 +8,18 @@ interface Props {
 }
 const Tasks: FC<Props> = ({ escenario, preguntas }) => {
     return (
-        <section className={s.Tareas}>
+        <section className={s.Tasks}>
             <h1>Tareas</h1>
             <p>Escenario: {escenario}</p>
+            <section className={s.TaskZone}>
             {
-                preguntas.map((e: any, index: any) => (<section key={index}>
+                preguntas.map((e: any, index: any) => (<article key={index} className={s.Task}>
                     <h1>Tarea {index}</h1>
-                    <h1>{e.texto}</h1>
+                    <h1 className={s.ToDo}>{e.texto}</h1>
                     <p>Duracion de la tarea: {e.tiempo}</p>
-                </section>))
+                </article>))
             }
+            </section>     
         </section>
     );
 }
